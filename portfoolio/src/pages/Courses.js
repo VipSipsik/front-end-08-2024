bcfa9a45e52865526c76f355930de72ada2e2ec8
VipSipsik <mirjamprass@gmail.com>
2024-08-18 21:04:09 +0300

    useState jätk - koolitused leht, kujundus

--- portfoolio/src/pages/Courses.js
+++ portfoolio/src/pages/Courses.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 function Courses() {
+  const [kursus, uuendaKursus] = useState("Valimata");
   return (
     <div>
         <Link to="/">
          <button>Tagasi</button>
         </Link>
-        <div>Kursuste sisu</div>
+        <div>
+          <button className={kursus === "udemy" ? "aktiivne" : "mitte-aktiivne"} onClick={() => uuendaKursus ("udemy")}>Udemy</button>
+          <button className={kursus === "coursera" ? "aktiivne" : "mitte-aktiivne"} onClick={() => uuendaKursus ("coursera")}>Coursera</button>
+          <button className={kursus === "codeacademy" ? "aktiivne" : "mitte-aktiivne"} onClick={() => uuendaKursus ("codeacademy")}>Codeacademy</button>
+          <button className={kursus === "udacity" ? "aktiivne" : "mitte-aktiivne"} onClick={() => uuendaKursus ("udacity")}>Udacity</button>
+          {kursus === "udemy" && <div>Siin on kirjeldus (loend) Udemy.com läbitud kursustest</div>}
+          {kursus === "coursera" && <div>Siin on kirjeldus (loend) Coursera.org läbitud kursustest</div>}
+          {kursus === "codeacademy" && <div>Siin on kirjeldus (loend) Codeacademy.com läbitud kursustest</div>}
+          {kursus === "udacity" && <div>Siin on kirjeldus (loend) Udacity.com läbitud kursustest</div>}
+        </div>
     </div>
   )
 }

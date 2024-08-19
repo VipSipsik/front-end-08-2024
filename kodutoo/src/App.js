5016308b6e5a2efe80cc55dfde459647cb84fd10
VipSipsik <mirjamprass@gmail.com>
2024-08-19 17:21:51 +0300

    Sisselogimise kuvamine

--- kodutoo/src/App.js
+++ kodutoo/src/App.js
@@ -4,10 +4,44 @@ import Kontakt from './pages/Kontakt';
 import Avaleht from './pages/Avaleht';
 import Meist from './pages/Meist';
 import Seaded from './pages/Seaded';
+import { useRef, useState } from 'react';
 
 function App() {
+  const [sisselogitud, muudaSisselogitud] = useState("ei");
+  const [sonum, muudaSonum] = useState("");
+  const kasutajaNimiRef = useRef();
+  const paroolRef = useRef();
+
+  const logiSisse = () => {
+  if (paroolRef.current.value === "123") {
+    muudaSisselogitud("jah");
+    muudaSonum(kasutajaNimiRef.current.value + " Mirjam ,Oled sisselogitud");
+  } else {
+    muudaSonum("Vale parool");
+  }
+}
+
+const logiValja = () => {
+  muudaSisselogitud("ei");
+  muudaSonum("");
+}
+
   return (
     <div className="App">
+      <div>{sonum}</div>
+    { sisselogitud === ("ei") && <div>
+      <label>Kasutajanimi</label> <br />
+      <input ref={kasutajaNimiRef} type="text" /> <br />
+      <label>Parool</label> <br />
+      <input ref={paroolRef} type="password" /> <br />
+    </div>}
+
+
+      { sisselogitud === "ei" && <button onClick={logiSisse}>Logi sisse</button>}
+      { sisselogitud === "jah" && <button onClick={logiValja}  >Logi välja</button>}
+      <br /><br />
+
+
       <div>--SIIN ON VARASEM KODUTÖÖ--</div>
 
       <Link to="/">

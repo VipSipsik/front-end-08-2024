d19752b70f140db589becead593d6b08b56fb8f4
VipSipsik <mirjamprass@gmail.com>
2024-08-19 16:20:43 +0300

    useState, buttons, if/else

--- kodutoo/src/pages/Kontakt.js
+++ kodutoo/src/pages/Kontakt.js
@@ -5,11 +5,13 @@ function Kontakt() {
     const [aadress, m22raAdress] = useState("Tallinn");
     const [telefon, m22raTelefon] = useState("55512345");
     const [email, m22raEmail] = useState("bla@aa.com");
+    const [ingliseKeelne, m22raIngliseKeelne] = useState("ei");
 
     const ingliseks = () => {
         m22raAdress("London");
         m22raTelefon("123123432");
         m22raEmail("london@london.com");
+        m22raIngliseKeelne("jah");
     }
 
 
@@ -19,8 +21,9 @@ function Kontakt() {
         <div> { telefon } </div>
         <div> { email } </div>
         <button onClick={ingliseks}>Muuda inglise keelseks</button>
+        { ingliseKeelne === "jah" && <div>Leht on inglise keelne</div>}
     </div>
-  )
+  );
 }
 
 export default Kontakt

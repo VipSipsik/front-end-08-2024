ae9b945eb7623ed625a02b940fcd065be23378b5
VipSipsik <mirjamprass@gmail.com>
2024-08-19 21:14:17 +0300

    tingim. ja muutujad. osa2, NB!

--- kodus-1/src/App.js
+++ kodus-1/src/App.js
@@ -1,18 +1,66 @@
-import {useState} from "react";
+import {useState, useRef} from "react";
 
 function App() {
   const [kasutajanimi, muudaKasutajanimi] = useState("Kasutaja1");
   const [kogus, muudaKogus] = useState(5);
   const [teade, muudaTeade] = useState("Tere");
   const [vanus, muudaVanus] = useState(65);
+  const [meil, muudaMeil] = useState("ei");
+  const [sonum, muudaSonum] = useState("");
+  const [nimi, muudaNimi] = useState("ei");
+  
+   
+  const emailRef = useRef();
+  const nimiRef = useRef();
+
+
+ // const sisestaEmail = () => {
+ //  if (emailRef.current.value === "12345") {
+ //   muudaMeil("jah");
+ //   muudaSonum(emailRef.current.value + ",e-mail sisestatud");
+ // } 
+ //   else {
+ //     muudaSonum("Vale e-mail");
+ //   }
+ // }
+
+  const sisestaNimi = () => {
+   if (nimiRef.current.value === "karlos") {
+    muudaNimi("jah");
+    muudaSonum("Nimi sisestatud");
+   } else {
+    muudaSonum("Vale nimi");
+   }
+  }
 
 
   return (
     <div classname="App">
+     <div>{ sonum }</div> <br />
+
+      <div>
+      <label>Nimi</label>
+      <input ref={nimiRef} type="text"  /> <br />
+      </div> 
+
+    { nimi === "ei" && <button onClick={sisestaNimi}>Sisesta nimi</button>}
+    { nimi === "jah" && <button onClick={() => muudaNimi("")}>Kustuta nimi</button>}
+      
+
+
+      
+         
+      {<div>
+      <label>E-mail</label> <br />
+      <input ref={emailRef} type="text" /><br />
+      </div>}
+
+      {/* {meil === "ei" && <button onClick={sisestaEmail}>Sisesta e-mail</button>} */}
+      {meil === "jah" && <button onClick={() => muudaMeil("jah")}>Sisselogitud</button>}
+      
       {kasutajanimi}
       <br />
-
-      <button onClick={() => muudaKasutajanimi("MuuKasutaja2")}>Muuda</button>
+        <button onClick={() => muudaKasutajanimi("MuuKasutaja2")}>Muuda</button>
       <br /><br />
       
       {kogus}

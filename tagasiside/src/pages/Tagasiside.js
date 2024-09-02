7187277d1ec25bcc8107484bc137e1330e9a938f
VipSipsik <mirjamprass@gmail.com>
2024-09-02 15:47:01 +0300

    arrays, kustutamine, uue lisamine

--- tagasiside/src/pages/Tagasiside.js
+++ tagasiside/src/pages/Tagasiside.js
@@ -1,12 +1,33 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 
 function Tagasiside() {
-    const [tagasisided, määraTagasisided] = useState(["Oli hea", "Huvitav", "Teistsugune", "Põnev"]);
+    const [tagasisided, määraTagasisided] = useState(["Oli hea", 
+      "Huvitav", "Teistsugune", "Põnev"]);
+    
+    const tagasisideRef = useRef();
+
+  function kustuta(index) {
+    tagasisided.splice(index,1);
+    määraTagasisided(tagasisided.slice());
+  }
+
+  function lisaUusTagasiside() {
+    tagasisided.push(tagasisideRef.current.value);
+    määraTagasisided(tagasisided.slice());
+  }
 
   return (
     <div>Tagasisided:
-     {tagasisided.map(element => <div>{element}</div>)}
-     </div>
+     {tagasisided.map((element, index) => 
+     <div>
+      <span>{element}</span>
+      <button onClick={() => kustuta(index)}>x</button>
+     </div>)}
+     <br /><br />
+     <label>Lisa uus tagasiside</label>
+     <input ref={tagasisideRef} type="text" />
+     <button onClick={() => lisaUusTagasiside()}>Sisesta</button>
+    </div>
   )
 }
 

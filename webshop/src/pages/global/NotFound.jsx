f063d4e6ce2a5853a3b0e4748f774511b136ce41
VipSipsik <mirjamprass@gmail.com>
2024-09-29 03:13:05 +0300

    Usetranslation and Link import

--- webshop/src/pages/global/NotFound.jsx
+++ webshop/src/pages/global/NotFound.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
 
 function NotFound() {
+  const { t } = useTranslation();
+
   return (
-    <div>NotFound</div>
+    <div>
+      <h3>404</h3>
+      <h1>{t('page-not-found')}</h1>
+      <Link to=" ">
+        <button className="btn"> {t("Homepage")}</button>
+      </Link>
+
+    </div>
   )
 }
 
